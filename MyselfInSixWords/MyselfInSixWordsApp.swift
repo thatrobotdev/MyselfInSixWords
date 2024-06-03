39bc99fa1b08ffd1646c17db60e28121ce911718
James Kerrane <hi@jameskerrane.com>
2024-06-03 16:41:56 -0600

    Initial Commit

---
+++ MyselfInSixWords/MyselfInSixWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyselfInSixWordsApp.swift
+//  MyselfInSixWords
+//
+//  Created by James Kerrane on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MyselfInSixWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
